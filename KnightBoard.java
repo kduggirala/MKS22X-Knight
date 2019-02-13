c778764c89fc3dae92bf7ecf42980627a5b2165a
Kiran Duggirala <kduggirala10@stuy.edu>
2019-02-13 10:30:47 -0500

    Created constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,11 +1,26 @@
 public class KnightBoard {
-	public KnightBoard(int startingRows,int startingCols) {}
+	private int r;
+	private int c;
+	private Coordinate[][] board;
+	private int[][] movesBoard;
+	public KnightBoard(int startingRows,int startingCols) {
+		r = startingRows;
+		c = startingCols;
+		board = new Coordinate[r][c];
+		movesBoard = new int[r][c];
+		for (int i  = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				board[r][c] = new Coordinate(r , c);
+			}
+		}
+	}
 
 	//Initialize the board to the correct size and make them all 0's 
 
 
-	public String toString() {}
-	see format for toString below
+	public String toString() {
+		StringBuffer boardString = new StringBuffer();
+	}
 	/*blank boards display 0's as underscores 
 you get a blank board if you never called solve or 
 when there is no solution
