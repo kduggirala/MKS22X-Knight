6db7ee2e7ec0d66786987ce9a99945ef57863716
Kiran Duggirala <kduggirala10@stuy.edu>
2019-02-25 11:17:05 -0500

    Tested solve on larger boards, still quick

--- KnightBoardTest.java
+++ KnightBoardTest.java
@@ -1,7 +1,7 @@
 
 public class KnightBoardTest {
 	public static void main(String[] args) {
-		KnightBoard k = new KnightBoard(8, 8);
+		KnightBoard k = new KnightBoard(9, 11);
 		k.solve(0, 0);
 		System.out.println(k);
 	}
